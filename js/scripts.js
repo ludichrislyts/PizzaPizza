ebdc76e785fb150a44ec81c9f1ffdbcce73d9254
Chris <ludichrislyts@gmail.com>
2015-09-19 06:36:25 -0700

    finally get addition to work

--- js/scripts.js
+++ js/scripts.js
@@ -25,25 +25,68 @@ Order.prototype.addToOrder = function(item){
 }
 Pizza.prototype.addTopping = function(topping){
     this.toppings.push(topping.name);
-    this.price += topping.price;
+    addPrice = parseFloat(topping.price).toFixed(2);
+    console.log(addPrice + " addPrice");
+    console.log((this.price + addPrice) + " parsed in addTopping");
+
+    this.price = Number(this.price) + Number(parseFloat(addPrice).toFixed(2));
 }
 $("#order-button").text("Begin Order");
 $(".begin-order").click(function(){
     $("#order-button").text("Making Pizza");
-    order = new Order;
+    $(".begin-order").prop("disabled", true);
     $(".selections").show();
+    $(".toppings-select").show();
+    $(".quantity").show();
+    $("#submit-order").show();
     beginOrder();
 });
 var beginOrder = function(){
-// selection class is the form holding all the pizza order options (size, toppings)
-    $("#size-select").on("click",function(event){
+    $(".selections").submit(function(event){
         event.preventDefault();
-        console.log($("#size-select option"));
-        console.log($(this));
-        pizza1 = new Pizza($("#size-select option").val());
-        //console.log(pizza1);
-});
+        // create new order
+        var order = new Order;
+        // create new size with name and price
+        var newSize = ($("input#size").attr("name"));
+        var newPiePrice = parseFloat($("input#size").attr("value")).toFixed(2);
+        var size = new PieSize(newSize, newPiePrice);
+        // create new pizza
+        var pizza = new Pizza(size);
+        var toppings = [];
+        console.log($(document.getElementById("toppings-select").getElementsByTagName("input")));
+        //console.log($(document.getElementById("toppings-select").children));
+        toppings = ($(document.getElementById("toppings-select").getElementsByTagName("input")));
+        console.log(toppings);
+        //toppings.getElementsByTagName("input");
+        // console.log(toppings);
+        // var numOfPossibleToppings = $(".toppings-select").getElementsById("topping");
+        // console.log(numOfPossibleToppings);
+
+        for(var i = 0; i < toppings.length; i++){
+            if(toppings[i].checked){
+                var name = $(toppings[i]).attr("name");
+                //console.log(parseFloat($(toppings[i].attr("value"))));
+                var price = parseFloat($(toppings[i]).val());
+                price = parseFloat(price).toFixed(2);
+                //.toFixed(2));
+                console.log(price + " price");
+                var topping = new Topping(name, price);
+                pizza.addTopping(topping);
+            }
+        }
+        console.log(pizza);
+
+    });
 }
+// selection class is the form holding all the pizza order options (size, toppings)
+    // $("#size-select").on("click",function(event){
+    //     event.preventDefault();
+    //     console.log($("#size-select option"));
+    //     console.log($(this));
+    //     pizza1 = new Pizza($("#size-select option").selected.val());
+    //     //console.log(pizza1);
+// });
+// }
 //var element = document.getElementById("size-select");
 //     var e = document.getElementById("ddlViewBy"); var strUser = e.options[e.selectedIndex].value;
 
